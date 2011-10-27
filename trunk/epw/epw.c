b7a1b2e0ddb846ac5120cb12e47ee775a3767082
ywangd <ywangd@7173c3f4-2921-a6f1-38a4-e29d01e29099>
2011-10-27 00:41:57 +0000

    Renamed to epw in the file contents.
    git-svn-id: http://coderplay.googlecode.com/svn@164 7173c3f4-2921-a6f1-38a4-e29d01e29099

--- trunk/epw/epw.c
+++ trunk/epw/epw.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "ipc.h"
+#include "epw.h"
 
 void *
 safe_malloc (size_t size)
--- trunk/epw/epw.y
+++ trunk/epw/epw.y
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include "ipc.h"
+#include "epw.h"
 
 extern FILE *yyin;
 
