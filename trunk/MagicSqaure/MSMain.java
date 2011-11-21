c5958c55e0f05a270327f63c814fb245614064cc
ywangd@gmail.com <ywangd@gmail.com@7173c3f4-2921-a6f1-38a4-e29d01e29099>
2011-11-21 05:13:20 +0000

    Really a mess with the msq.
    git-svn-id: http://coderplay.googlecode.com/svn@180 7173c3f4-2921-a6f1-38a4-e29d01e29099

--- trunk/MagicSqaure/MSMain.java
+++ trunk/MagicSqaure/MSMain.java
@@ -59,59 +59,183 @@ public class MSMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		
+
 		if (parseOpt(args) == 0)
 			System.exit(1);
-		
+
 		int ii, jj;
 		int lsidx, rsidx; // left/right row index
 		int leidx, reidx; // left/right element index
+		int lgval, rgval;
+		int lgidx, rgidx;
 
-		
+		int nsize2 = nsize + nsize;
+		int nsums = nsize2 + 2;
+
 		System.out.println("-n " + nsize);
 		System.out.println("-c " + isConstrained);
 		System.out.println("-p " + prow + " " + pcol);
 
 		// Setup common variables, e.g. the initial matrix, the goalsum
 		MSMatrix mat = new MSMatrix(nsize, isConstrained, prow, pcol);
+		System.out.println(mat.getUpbound0());
 		mat.print();
 		mat.printsums();
-		Integer [] idxsort = mat.getIdxSortSums();
-		for (Integer idx: idxsort) {
-			System.out.print(" "+idx);
+		System.out.println(mat.getGoalsum());
+		System.out.println(mat.getDistance());
+		Integer[] idxsort = mat.getIdxSortSums();
+		int nuniquesums;
+		boolean[] fcons = mat.getFcons();
+		int[] idxforsum1 = new int[nsize];
+		int[] idxforsum2 = new int[nsize];
+		Integer[] idxforsum1_sort;
+		Integer[] idxforsum2_sort;
+
+		for (Integer idx : idxsort) {
+			System.out.print(" " + idx);
 		}
 		System.out.println();
 
 		// calculate the sums and stats of the initial matrix
 
+		int minv, maxv;
+		boolean findit, candidateSwap;
+		int temp;
+		temp = 100;
+
 		// main loop of the search
 		do {
-			
+
 			idxsort = mat.getIdxSortSums();
+			nuniquesums = mat.countNuniqueSumsExceptZero(idxsort);
 
 			// Generate and enqueue a new matrix
-			if (mat.getDistance() > mat.getUpbound0()) { // Fast descent to low state space
-				for (ii=0;ii<nsize+nsize+2;ii++) {
-					
-					for (jj=0;jj<nsize+nsize;jj++) {
-					
+			if (mat.getDistance() > mat.getUpbound0()) { // Fast descent to low
+															// state space
+				findit = false;
+				for (ii = 0; ii < nsums; ii++) {
+
+					// The sums to work on
+					lsidx = ii / 2;
+					rsidx = nsums - 1 - (ii / 2 + ii % 2);
+					System.out.println(lsidx + ", " + rsidx);
+					if (lsidx >= rsidx) {
+						System.out.println("Error!");
+						mat.print();
+						mat.printsums();
+						System.out.println(mat.getDistance());
+						System.exit(1);
 					}
+
+					idxforsum1_sort = mat
+							.getSortedIndexOfSingleSumElements(idxsort[lsidx]);
+					idxforsum2_sort = mat
+							.getSortedIndexOfSingleSumElements(idxsort[rsidx]);
+
+					for (jj = 0; jj < nsize2; jj++) {
+						leidx = jj / 2;
+						reidx = nsize2 - 1 - (jj / 2 + jj % 2) - nsize;
+
+						if (leidx >= nsize || reidx < 0)
+							break;
+
+						if (mat.isIndexConstrained(idxforsum1_sort[leidx]
+								.intValue())
+								|| mat.isIndexConstrained(idxforsum2_sort[reidx]
+										.intValue())) {
+							continue;
+						}
+
+						// Try the swap
+						mat.update(idxforsum1_sort[leidx].intValue(),
+								idxforsum2_sort[reidx].intValue());
+
+						// Test the swap
+						if (mat.verifyUpdateSimple()
+								&& !closed.contains(mat.getMatrix())) {
+							findit = true;
+							System.out.println(idxforsum1_sort[leidx] + ":: "
+									+ idxforsum2_sort[reidx]);
+							break;
+						} else {
+							mat.revertUpdate();
+						}
+					}
+					if (findit)
+						break;
 				}
-				
-				
-				
+
 			} else {
+
+				lsidx = idxsort[0];
+				rsidx = idxsort[idxsort.length - 1];
+				idxforsum1_sort = mat.getSortedIndexOfSingleSumElements(lsidx);
+				idxforsum2_sort = mat.getSortedIndexOfSingleSumElements(rsidx);
+
 				// Perform either paired swap or simple swap in low state space
+				for (ii = 0; ii < nsize2; ii++) {
+					leidx = ii / 2;
+					reidx = nsize2 - 1 - (ii / 2 + ii % 2) - nsize;
+
+					if (leidx >= nsize || reidx < 0) {
+						break;
+					}
+
+					// Constrained
+					if (mat.isIndexConstrained(idxforsum1_sort[leidx]
+							.intValue())
+							|| mat.isIndexConstrained(idxforsum2_sort[reidx]
+									.intValue())) {
+						continue;
+					}
+
+					lgval = mat.getSums()[lsidx] - mat.getGoalsum();
+					rgval = mat.getSums()[rsidx] - mat.getGoalsum();
+
+					lgidx = mat.getIndexForElement(lgval);
+					rgidx = mat.getIndexForElement(rgval);
+
+					candidateSwap = true;
+					if (lgidx != -1 && rgidx != -1) {
+						for (jj = 0; jj < nsize; jj++) {
+							if (lgidx == idxforsum1_sort[jj]
+									|| lgidx == idxforsum2_sort[jj]
+									|| rgidx == idxforsum1_sort[jj]
+									|| rgidx == idxforsum2_sort[jj]) {
+
+								candidateSwap = false;
+								break;
+							}
+						}
+					}
+
+					if (candidateSwap) {
+						// try paired swap
+						mat.updatePairedSwap(idxforsum1_sort[leidx].intValue(),
+								lgidx, idxforsum1_sort[leidx].intValue(), rgidx);
+					} else {
+						// try simple swap
+						mat.update(idxforsum1_sort[leidx].intValue(),
+								idxforsum2_sort[reidx].intValue());
+					}
+
+				}
 
 			}
 
+			closed.add(mat.getMatrix().clone());
+
 			// update the sums and stats for the new matrix based on the old
 			// matrix data
-
-		} while (false); // while (mat.getDistance() != 0);
+			temp--;
+			System.out.println(temp);
+		} while (temp > 0); // while (mat.getDistance() != 0);
 
 		// end here
+
+		mat.print();
+		mat.printsums();
+		System.out.println(mat.getDistance());
 
 	}
 
--- trunk/MagicSqaure/MSMatrix.java
+++ trunk/MagicSqaure/MSMatrix.java
@@ -3,6 +3,7 @@
  */
 package src;
 
+import java.nio.IntBuffer;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -13,6 +14,7 @@ import java.util.Comparator;
 public class MSMatrix {
 
 	private int[] matrix;
+
 	private int nsize;
 	private int nsize2;
 	private int nelements;
@@ -25,6 +27,10 @@ public class MSMatrix {
 	private int[] idxSumChange = new int[4];
 	private long upbound0;
 
+	private int[] matrixOld;
+	private long distanceOld;
+	private int[] sumsOld;
+
 	public MSMatrix(int nsize, boolean isConstrained, int prow, int pcol) {
 
 		int ii, jj;
@@ -36,12 +42,12 @@ public class MSMatrix {
 		sums = new int[nsize2 + 2];
 		idxs = new Integer[nsize2 + 2];
 		goalsum = (nelements + 1) / 2 * nsize;
-		upbound0 = 6*(nelements-1)*(nelements-1);
+		upbound0 = 6 * (nelements - 1) * (nelements - 1);
 		idxforsum = new int[nsize2 + 2][nsize];
+		fcons = new boolean[nelements];
 
 		// Initialise the matrix elements
 		if (isConstrained) {
-			fcons = new boolean[nelements];
 			int idxpin = prow * nsize + pcol;
 			for (ii = 0; ii < 3; ii++) {
 				for (jj = 0; jj < 3; jj++) {
@@ -68,6 +74,10 @@ public class MSMatrix {
 		calcDistance();
 	}
 
+	public boolean isIndexConstrained(int idx) {
+		return fcons[idx];
+	}
+
 	/**
 	 * Calculate the summations of every rows, columns and two diagonals.
 	 */
@@ -75,11 +85,11 @@ public class MSMatrix {
 		int ii, jj;
 		int[] indices;
 
-		for (ii=0;ii<nsize2+2;ii++) {
+		for (ii = 0; ii < nsize2 + 2; ii++) {
 			indices = idxforsum[ii];
 			idxs[ii] = ii;
 			sums[ii] = 0;
-			for (jj=0;jj<nsize;jj++) {
+			for (jj = 0; jj < nsize; jj++) {
 				sums[ii] += matrix[indices[jj]];
 			}
 		}
@@ -92,6 +102,9 @@ public class MSMatrix {
 		}
 	}
 
+	/**
+	 * Find out which sums that are affected for a given element.
+	 */
 	private int updateIdxSumChange(int idx) {
 		int q, m;
 		int nchanges = 2;
@@ -125,6 +138,10 @@ public class MSMatrix {
 		int dev;
 		int diff;
 
+		matrixOld = matrix.clone();
+		distanceOld = distance;
+		sumsOld = sums.clone();
+
 		diff = matrix[idx1] - matrix[idx2];
 		for (ii = 0; ii < updateIdxSumChange(idx1); ii++) {
 			dev = sums[idxSumChange[ii]] - goalsum;
@@ -140,6 +157,81 @@ public class MSMatrix {
 			dev += diff;
 			distance += dev * dev;
 		}
+		matrix[idx1] -= diff;
+		matrix[idx2] += diff;
+	}
+
+	public boolean verifyUpdateSimple() {
+		if (distance < distanceOld)
+			return true;
+		else
+			return false;
+	}
+	
+	public void updatePairedSwap(int idx1, int idxg1, int idx2, int idxg2) {
+		int ii;
+		int dev;
+		int diff;
+
+		matrixOld = matrix.clone();
+		distanceOld = distance;
+		sumsOld = sums.clone();
+
+		diff = matrix[idx1] - matrix[idxg1];
+		for (ii = 0; ii < updateIdxSumChange(idx1); ii++) {
+			dev = sums[idxSumChange[ii]] - goalsum;
+			distance -= dev * dev;
+			sums[idxSumChange[ii]] -= diff;
+			dev -= diff;
+			distance += dev * dev;
+		}
+		for (ii = 0; ii < updateIdxSumChange(idxg1); ii++) {
+			dev = sums[idxSumChange[ii]] - goalsum;
+			distance -= dev * dev;
+			sums[idxSumChange[ii]] += diff;
+			dev += diff;
+			distance += dev * dev;
+		}
+		matrix[idx1] -= diff;
+		matrix[idxg1] += diff;
+		
+		diff = matrix[idx2] - matrix[idxg2];
+		for (ii = 0; ii < updateIdxSumChange(idx2); ii++) {
+			dev = sums[idxSumChange[ii]] - goalsum;
+			distance -= dev * dev;
+			sums[idxSumChange[ii]] -= diff;
+			dev -= diff;
+			distance += dev * dev;
+		}
+		for (ii = 0; ii < updateIdxSumChange(idxg2); ii++) {
+			dev = sums[idxSumChange[ii]] - goalsum;
+			distance -= dev * dev;
+			sums[idxSumChange[ii]] += diff;
+			dev += diff;
+			distance += dev * dev;
+		}
+		matrix[idx2] -= diff;
+		matrix[idxg2] += diff;
+		
+	}
+	
+	
+	public boolean verifyUpdatePairedSwap() {
+		if (distance < distanceOld)
+			return true;
+		else
+			return false;
+	}
+
+	public void revertUpdate() {
+		int ii;
+		for (ii = 0; ii < nelements; ii++) {
+			matrix[ii] = matrixOld[ii];
+		}
+		distance = distanceOld;
+		for (ii = 0; ii < sums.length; ii++) {
+			sums[ii] = sumsOld[ii];
+		}
 	}
 
 	public Integer[] getIdxSortSums() {
@@ -150,7 +242,34 @@ public class MSMatrix {
 				return sums[i1] - sums[i2];
 			}
 		});
+
 		return idxsort;
+	}
+
+	/**
+	 * Count the number of unique values of the sums array except zeros.
+	 * 
+	 * @param idxsort
+	 * @return
+	 */
+	public int countNuniqueSumsExceptZero(Integer[] idxsort) {
+		int ii, count, thesum, exceptZero;
+
+		thesum = sums[idxsort[0]];
+		count = 1;
+		exceptZero = 0;
+		for (ii = 1; ii < sums.length; ii++) {
+			if (sums[ii] == 0)
+				exceptZero = 1;
+			if (thesum == sums[ii]) {
+				continue;
+			} else {
+				thesum = sums[ii];
+				count++;
+			}
+		}
+		count -= exceptZero;
+		return count;
 	}
 
 	public void populateIdxforsum() {
@@ -201,6 +320,17 @@ public class MSMatrix {
 		return matrix;
 	}
 
+	public int getIndexForElement(int value) {
+		int ii;
+		if (value <= 0 || value > nelements)
+			return -1;
+		for (ii = 0; ii < nelements; ii++) {
+			if (value == matrix[ii])
+				return ii;
+		}
+		return -1;
+	}
+
 	public long getDistance() {
 		return distance;
 	}
@@ -208,14 +338,35 @@ public class MSMatrix {
 	public int[] getSums() {
 		return sums;
 	}
-	
+
 	public int[] getIndicesOfSingleSum(int idxsum) {
 		return idxforsum[idxsum];
+	}
+
+	public Integer[] getSortedIndexOfSingleSumElements(int idxsum) {
+		Integer[] idxforsum_sort = new Integer[nsize];
+		for (int ii = 0; ii < nsize; ii++) {
+			idxforsum_sort[ii] = idxforsum[idxsum][ii];
+		}
+		Arrays.sort(idxforsum_sort, new Comparator<Integer>() {
+			@Override
+			public int compare(final Integer i1, final Integer i2) {
+				return matrix[i1] - matrix[i2];
+			}
+		});
+		return idxforsum_sort;
 	}
 
 	public long getUpbound0() {
 		return upbound0;
 	}
 
-	
+	public boolean[] getFcons() {
+		return fcons;
+	}
+
+	public int getGoalsum() {
+		return goalsum;
+	}
+
 }
