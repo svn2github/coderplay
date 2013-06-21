d840916a09a75127165363e1e92c7e77677e6f0b
ywangd@gmail.com <ywangd@gmail.com@7173c3f4-2921-a6f1-38a4-e29d01e29099>
2013-06-21 05:01:17 +0000

    Changed the default input filename to [PROMPT]
    git-svn-id: http://coderplay.googlecode.com/svn@284 7173c3f4-2921-a6f1-38a4-e29d01e29099

--- trunk/Emma/file.py
+++ trunk/Emma/file.py
@@ -1,6 +1,6 @@
 
 class Line():
-    def __init__(self, text, line_number=1, filename=None):
+    def __init__(self, text, line_number=1, filename='[PROMT]'):
         self.text = text
         self.line_number = line_number
         self.filename = filename
