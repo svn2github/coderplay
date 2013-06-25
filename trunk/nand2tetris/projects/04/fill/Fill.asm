1046bd84423e5a476749f8855e0a0f54eece136b
ywangd@gmail.com <ywangd@gmail.com@7173c3f4-2921-a6f1-38a4-e29d01e29099>
2013-06-25 04:55:19 +0000

    Finished nand Project 4.
    git-svn-id: http://coderplay.googlecode.com/svn@299 7173c3f4-2921-a6f1-38a4-e29d01e29099

--- trunk/nand2tetris/projects/04/fill/Fill.asm
+++ trunk/nand2tetris/projects/04/fill/Fill.asm
@@ -9,3 +9,64 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+// We blacken or whiten the screen word by word.
+// 16 bit each time we operate. 
+// I am not sure how we can turn on/off each individual
+// bit instead of each word, or maybe it is rather complicated.
+
+    @i              // The counter of words
+    M=0             // reset to zero
+
+(LOOP)
+    @KBD    // listen to the keyboard
+    D=M
+
+    @WHITE
+    D;JEQ // If no key is pressed we white stuff
+
+    // else we black stuff
+
+    @i          
+    D=M
+    @8192   // Max number of screen word 16*512
+    D=D-A   // Check if the counter has reach its maximum value
+
+    @LOOP   // If it is we do nothing, everythin is blacken'd
+    D;JGE   
+
+    @i      // Get the correct screen address
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1    // Blacken the word
+    
+    @i
+    M=M+1   // increase the counter
+    
+    @LOOP   // Go back and listen to the keyboard again.
+    0;JMP
+
+(WHITE)
+    @i
+    D=M     // Check if the counter has reach its minimum 0
+    
+    @LOOP   // if it does we do nothing, everything is whiten'd
+    D;JLT
+
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=0     // whiten the word
+
+    @i
+    M=M-1   // decrease the counter
+
+    @LOOP
+    0;JMP
+
+    
+    
+
+    
--- trunk/nand2tetris/projects/04/mult/Mult.asm
+++ trunk/nand2tetris/projects/04/mult/Mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+// R2 = R0 * R1
+    @R2
+    M=0     // reset M[R2] to 0
+    @R1     // Number of additions
+    D=M
+    @i      // counter
+    M=D     // set the counter to number of additions
+(LOOP)
+    @i
+    D=M     // D = counter
+    @END
+    D;JEQ   // if counter is 0, end the calculation
+    @i
+    M=M-1   // decrease the counter by 1 
+    @R2
+    D=M     // load the previous stored product
+    @R0
+    D=D+M   // accumulate M[R0] one more time
+    @R2
+    M=D     // save product back to M[R2]
+    @LOOP
+    0;JMP   // Goto LOOP for more addition
+(END)
+    @END
+    0;JMP   // infinite loops for ending
+    
