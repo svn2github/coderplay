154280f2fc6061a934ea4d94c0a3088dc9b50d13
ywangd@gmail.com <ywangd@gmail.com@7173c3f4-2921-a6f1-38a4-e29d01e29099>
2013-07-09 01:41:30 +0000

    Kinda lost in the vm implementation. Need more readings.
    git-svn-id: http://coderplay.googlecode.com/svn@335 7173c3f4-2921-a6f1-38a4-e29d01e29099

--- trunk/Emma/epw_bytecode.py
+++ trunk/Emma/epw_bytecode.py
@@ -1,3 +1,74 @@
 
 ''' Bytecode class for Emma language.
 '''
+
+OP_PUSH     = 0
+OP_POP      = 1
+OP_LABEL    = 2
+OP_JUMP     = 3
+OP_FJUMP    = 4
+OP_FUNCTION = 5
+OP_RETURN   = 6
+OP_CALL     = 7
+OP_ADD      = 8
+OP_SUB      = 9
+OP_MUL      = 10
+OP_DIV      = 11
+OP_MOD      = 12
+OP_GT       = 13
+OP_GE       = 14
+OP_EQ       = 15
+OP_LE       = 16
+OP_LT       = 17
+OP_NE       = 18
+OP_AND      = 19
+OP_OR       = 20
+OP_XOR      = 21
+OP_NOT      = 22
+OP_NEG      = 23
+
+opcode_dict = {}
+opcode_dict[OP_PUSH] = 'push'
+opcode_dict[OP_POP] = 'pop'
+opcode_dict[OP_LABEL] = 'label'
+opcode_dict[OP_JUMP] = 'jump'
+opcode_dict[OP_FJUMP] = 'fjump'
+opcode_dict[OP_FUNCTION] = 'function'
+opcode_dict[OP_RETURN] = 'return'
+opcode_dict[OP_CALL] = 'call'
+opcode_dict[OP_ADD] = 'add'
+opcode_dict[OP_SUB] = 'sub'
+opcode_dict[OP_MUL] = 'mul'
+opcode_dict[OP_DIV] = 'div'
+opcode_dict[OP_MOD] = 'mod'
+opcode_dict[OP_GT] = 'gt'
+opcode_dict[OP_GE] = 'ge'
+opcode_dict[OP_EQ] = 'eq'
+opcode_dict[OP_LE] = 'le'
+opcode_dict[OP_LT] = 'lt'
+opcode_dict[OP_LE] = 'le'
+opcode_dict[OP_NE] = 'ne'
+opcode_dict[OP_AND] = 'and'
+opcode_dict[OP_OR] = 'or'
+opcode_dict[OP_XOR] = 'xor'
+opcode_dict[OP_NOT] = 'not'
+opcode_dict[OP_NEG] = 'neg'
+
+def opcode2str(opcode):
+    return opcode_dict[opcode]
+
+M_CONSTANT      = 'constant'
+M_LOCAL         = 'local'
+M_ARG           = 'argument'
+M_TEMP          = 'temp'
+
+
+class Instruction(object):
+
+    def __init__(self, opcode, segment=None, index=None):
+        self.opcode = opcode
+        self.segment = segment
+        self.index = index
+
+
+
--- trunk/Emma/epw_compiler.py
+++ trunk/Emma/epw_compiler.py
@@ -1,9 +1,22 @@
 import sys
+from epw_bytecode import *
 
 ''' Driver of Emma language compiler. 
     The results of compilation is the virtual machine code without line numbers.
     The line numbers will be generated in subsequent assembler phase.
 '''
+
+class ControlFlowGraph(object):
+
+    def __init__(self):
+        self.args = []
+        self.locals = []
+        self.instrlist = []
+
+    def addInstruction(self, instr):
+        self.instrlist.append(instr)
+
+            
 
 
 def compile(ast_node):
