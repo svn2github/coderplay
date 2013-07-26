35eff450069893957d223e52eafcc0c82c94fd74
ywangd@gmail.com <ywangd@gmail.com@7173c3f4-2921-a6f1-38a4-e29d01e29099>
2013-07-26 13:44:56 +0000

    git-svn-id: http://coderplay.googlecode.com/svn@387 7173c3f4-2921-a6f1-38a4-e29d01e29099

---
+++ trunk/Emma/src/wordstable.h
@@ -0,0 +1,41 @@
+#ifndef _WORDSTABLE_H
+#define _WORDSTABLE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char* lexeme;
+    int tag;
+} Word;
+
+typedef struct {
+    char* lexeme; // a string
+    Word* word;
+} WtEntry;
+
+typedef struct {
+    /* size of the words table */
+    int size;        
+    /* number of items in the words table */
+    int nwords;       
+    WtEntry** table;  /* table entries */
+} Wordstable;
+
+Wordstable* wt_create(unsigned int size);
+
+void wt_free(Wordstable* htable);
+
+unsigned long wt_hash(char* lexeme);
+
+Wordstable* wt_install(Wordstable* htable, char* lexeme, Word* word);
+
+void wt_delete(Wordstable* htable,char* lexeme);
+
+Word* wt_lookup(Wordstable* htable,char* lexeme);
+
+Wordstable* wt_rehash(Wordstable* htable);
+
+void wt_dump(Wordstable* htable);
+
+#endif
